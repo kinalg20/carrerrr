3df5b7393850bb16110caf5240e8d58bf71402dc
corniceinfosoft <corniceinfo.soft.sol@gmail.com>
2021-10-19 18:01:52 +0530

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'career-p';
+  Isshowmodel :boolean=false;
+  string:any;
+  applyfunc(string:any){
+    this.Isshowmodel=!this.Isshowmodel
+    this.string=string
+  }
+
+  close(){
+    this.Isshowmodel=false
+  }
+}
